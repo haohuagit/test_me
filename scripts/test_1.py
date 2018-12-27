b03278afd30c2635c369767707bb6fe2501659db
git <git@qq.com>
2018-12-27 10:56:31 +0800

    自己的

---
+++ scripts/test_1.py
@@ -0,0 +1,16 @@
+
+
+
+class Test1:
+    def test_a(self):
+        print('\n1111')
+        assert 1
+
+
+    def test_b(self):
+        print('\n2222')
+
+        print('\n333')
+
+        print('\n444')
+        assert 1
